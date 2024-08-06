ca754a432f14150e1fb1258c469aaa1fa89b9a5a
Phạm Long Vu <86700532+phamlongvu0205@users.noreply.github.com>
2024-08-06 08:59:06 +0700

    Update import_file.py

--- pages/import_file.py
+++ pages/import_file.py
@@ -1,20 +1,3 @@
 import pandas as pd
 from package import all
 import streamlit as st
-
-file_path = "raw_data/raw_data.xlsx"
-
-# Read data from Excel
-cars = pd.read_excel(file_path, header=None).squeeze()
-
-# Calculate statistics
-desc = all.descriptive_stats(cars)
-desc1 = pd.DataFrame([desc]).T
-
-col1, col2 = st.columns(2)
-
-with col1:
-    st.write(cars)
-    
-with col2:
-    st.write(desc1)
