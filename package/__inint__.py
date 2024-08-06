b107764b4c7ab9675c989bc4d0e84a8cf8f42811
Phạm Long Vu <86700532+phamlongvu0205@users.noreply.github.com>
2024-08-06 08:25:34 +0700

    Create __inint__.py

---
+++ package/__inint__.py
@@ -0,0 +1,11 @@
+# Copyright 2024 Pham Long Vu @ GitHub
+# See LICENSE for details.
+
+__author__ = "Pham Long Vu @phamlongvu0205 in GitHub"
+
+from .desc_stats import all
+
+__all__ = [
+    'plot_stats',
+    'all',
+]
